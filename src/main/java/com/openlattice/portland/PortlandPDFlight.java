5b8e938c64c2ac0c09ed5047d8af3e1e048034ca
Joke Durnez <joke.durnez@gmail.com>
2018-04-24 17:18:50 -0700

    update working version with new backend

--- src/main/java/com/openlattice/portland/PortlandPDFlight.java
+++ src/main/java/com/openlattice/portland/PortlandPDFlight.java
@@ -1,5 +1,8 @@
 package com.openlattice.portland;
 
+import com.dataloom.mappers.ObjectMappers;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.openlattice.client.RetrofitFactory;
 import com.openlattice.shuttle.Flight;
 import com.openlattice.shuttle.Shuttle;
@@ -8,11 +11,13 @@ import com.openlattice.shuttle.dates.DateTimeHelper;
 import com.openlattice.shuttle.dates.TimeZones;
 import com.openlattice.shuttle.payload.Payload;
 import com.openlattice.shuttle.payload.SimplePayload;
+import com.openlattice.shuttle.util.Parsers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class PortlandPDFlight {
 
@@ -25,7 +30,7 @@ public class PortlandPDFlight {
     private static final DateTimeHelper dtHelper = new DateTimeHelper( TimeZones.America_NewYork,
             "yyyy-MM-dd" );
 
-    public static void main( String[] args ) throws InterruptedException {
+    public static void main( String[] args ) throws InterruptedException, JsonProcessingException {
 /*
          * It's worth noting that we are omitting validation such as making sure enough args were passed in, checking
          * existence of the file, and making sure authentication was successful. A failure in one of these cases
@@ -44,17 +49,16 @@ public class PortlandPDFlight {
         Flight arFlight = Flight.newFlight()
                 .createEntities()
 
-                .addEntity( "PortlandPDArresteePerson" )  //variable name within flight. Doesn't have to match anything anywhere else
-                    .to( "PortlandPDArresteePerson" )       //name of entity set belonging to
+                .addEntity( "PortlandPDArrestee" )  //variable name within flight. Doesn't have to match anything anywhere else
+                    .to( "PortlandPDArrestee" )       //name of entity set belonging to
                     .addProperty( "nc.PersonGivenName", "Arrestee First Name" )           //arrest report number
                     .addProperty( "nc.PersonMiddleName", "Arrestee Middle Name" )
                     .addProperty( "nc.PersonSurName", "Arrestee Last Name" )
                     .addProperty( "nc.PersonRace" ).value( PortlandPDFlight::standardRace ).ok()
                     .addProperty( "nc.PersonEthnicity" ).value( PortlandPDFlight::standardEthnicity ).ok()
-//                    .addProperty( "nc.SubjectIdentification" , "Arrest Charges Case #" )
                     .addProperty( "nc.PersonSex" ).value( PortlandPDFlight::standardSex ).ok()
                     .addProperty( "nc.PersonBirthDate" )
-                    .value( row -> dtHelper.parse( row
+                    .value( row -> dtHelper.parseDate( row
                             .getAs( "Arrestee DOB Date" ) ) )   //these rows are shorthand for a full function .value as below
                     .ok()
                 .endEntity()
@@ -68,56 +72,59 @@ public class PortlandPDFlight {
                     .to("PortlandPDOfficerPerson")
                     .addProperty( "nc.PersonGivenName", "Arrresting Ofc Frist Name" )    //unique ID for address
                     .addProperty( "nc.PersonSurName", "Arresting Ofc Last Name" )    //unique ID for address
-//                    .addProperty( "nc.SubjectIdentification" , "Arresting Officer #" )
+                    .addProperty( "nc.SubjectIdentification" , "Arresting Officer #" )
                 .endEntity()
 
-                .addEntity( "PortlandPDOfficerID" )
-                    .to("PortlandPDOfficerID")
+                .addEntity( "PortlandPDOfficer" )
+                    .to("PortlandPDOfficer")
                     .addProperty( "publicsafety.officerid", "Arresting Officer #" )    //unique ID for address
                  .endEntity()
+
                 .endEntities()
-//
+
                 .createAssociations()
                 .addAssociation( "PortlandPDArrestedIn" )
                 .to( "PortlandPDArrestedIn" )
-                .fromEntity( "PortlandPDArresteePerson" )
+                .fromEntity( "PortlandPDArrestee" )
                 .toEntity( "PortlandPDIncident" )
-                    .addProperty( "ol.arrestdatetime" )
-                    .value( row -> dtHelper.parse( row
-                            .getAs( "Arrest Date" ) ) )   //these rows are shorthand for a full function .value as below
-                    .ok()
+                .entityIdGenerator(row -> UUID.randomUUID().toString())
+//                    .addProperty( "ol.arrestdate" )
+//                    .value( row -> dtHelper.parseDate( row
+//                            .getAs( "Arrest Date" ) ) )   //these rows are shorthand for a full function .value as below
+//                    .ok()
                     .addProperty( "location.address" ).value( PortlandPDFlight::standardAddress ).ok()
                     .addProperty( "ol.arrestcategory", "ARREST TYPE (186) eng" )
                     .addProperty( "criminaljustice.casenumber", "Arrest Charges Case #" )
                     .addProperty( "criminaljustice.nibrs", "UCR OFFN CODE (47) eng" )
-                    .addProperty( "general.ChargeSequenceID", "Adult Arrest Charge Seq #" )
-                    .addProperty( "ol.personsequencenumber", "Arrest Person Sequence Number" )
+                    .addProperty( "ol.numberofcounts", "Adult Arrest Charge Seq #" )
                     .addProperty( "ol.mapreference", "Map Ref# of Arst" )
-                    .addProperty( "person.ageatevent", "Arrestee Age" )
+                    .addProperty( "person.ageatevent").value( row -> Parsers.parseInt( row.getAs("Arrestee Age")) ).ok()
                     .addProperty( "j.ArrestCharge", "ARREST STATE CHARGE (7) eng" )
                 .endAssociation()
 
                 .addAssociation( "PortlandPDArrestedBy" )
                 .to("PortlandPDArrestedBy")
-                .fromEntity("PortlandPDArresteePerson")
-                .toEntity( "PortlandPDOfficerID" )
+                .fromEntity("PortlandPDArrestee")
+                .toEntity( "PortlandPDOfficer" )
                 .addProperty( "general.StringID", "Arrest Incident Number" )
                 .endAssociation()
 
-                .addAssociation( "PortlandPDPersonBecomesOfficial" )
-                .to("PortlandPDPersonBecomesOfficial")
+                .addAssociation( "PortlandPDBecomes" )
+                .to("PortlandPDBecomes")
                 .fromEntity( "PortlandPDOfficerPerson" )
-                .toEntity( "PortlandPDOfficerID" )
+                .toEntity( "PortlandPDOfficer" )
                 .addProperty( "nc.SubjectIdentification", "Arrest Incident Number" )
                 .endAssociation()
 
                 .endAssociations()
                 .done();
 
+        ObjectMapper mapper = ObjectMappers.getYamlMapper();
 
+//        logger.info("This is the JSON for the flight. {}", mapper.writeValueAsString(arFlight));
         // add all the flights to flights
         flights.put( arFlight, arPayload );
-
+//
         // Send your flight plan to Shuttle and complete integration
         Shuttle shuttle = new Shuttle( environment, jwtToken );
         shuttle.launchPayloadFlight( flights );
@@ -129,22 +136,24 @@ public class PortlandPDFlight {
 
     public static String standardRace( Row row ) {
         String sr = row.getAs( "ARRESTEE RACE (11) eng" );
+        String race = "";
         if ( sr != null ) {
             if ( sr.equals( "ASIAN OR PACIFIC ISLANDER" ) ) {
-                return "asian/pacisland";
+                race = "asian/pacisland";
             } else if ( sr.equals( "BLACK" ) ) {
-                return "black";
+                race = "black";
             } else if ( sr.equals( "UNKNOWN" ) | sr.equals( "HISPANIC" ) | sr.equals( "AMERICAN INDIAN / ALASKAN NATIVE" ) | sr.equals( "* Race *" )) {
-                return "";
+                race = "";
             } else if ( sr.equals( "WHITE" ) ) {
-                return "white";
+                race = "white";
             }
         }
-        return null;
+        return race;
     }
 
     public static String standardEthnicity( Row row ) {
         String sr = row.getAs( "ARRESTEE ETHNIC (32) eng" );
+
         if ( sr != null ) {
             if ( sr.equals( "HISPANIC" ) ) {
                 return "hispanic";
@@ -158,23 +167,23 @@ public class PortlandPDFlight {
     }
 
     public static String standardSex( Row row ) {
+        String sex = "";
         String sr = row.getAs( "ARRESTEE SEX (10) eng" );
         if ( sr != null ) {
             if ( sr.equals( "FEMALE" ) ) {
-                return "F";
+                sex= "F";
             } else if ( sr.equals( "MALE" ) ) {
-                return "M";
-            } else if ( sr.equals( "UNKNOWN" ) | sr.equals( "* Sex *" )) {
-                return "";
+                sex= "M";
             }
         }
-        return null;
+        return sex;
     }
 
     public static String standardAddress( Row row ) {
         String strnum = row.getAs( "Arrest Location St#" );
         String strname = row.getAs( "Arrest Location Whole Street Name" );
-        return strnum + strname;
+        String total = strnum + " " + strname;
+        return total;
     }
 
 }
